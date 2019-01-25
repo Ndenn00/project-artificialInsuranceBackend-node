4adb0caafbc735d3c164fd6452e267585c4560af
Nial Dennison <ndennison01@qub.ac.uk>
2019-01-25 17:04:18 +0000

    api router set up and working

--- app.js
+++ app.js
@@ -2,3 +2,16 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose'); 
+
+mongoose.connect('mongodb://localhost/artins'); 
+var db = mongoose.connection; 
+
+var apiRouter = require('./routes/api');
+app.use(bodyParser.json());
+
+app.use('/api', apiRouter);
+
+
+
+app.listen(3000);
+console.log('Running on port 3000') 
