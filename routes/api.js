f2228f815c9d1d5f05aa763789838f3c697299e6
Nial Dennison <ndennison01@qub.ac.uk>
2019-03-16 21:14:34 +0000

    added path to api.js

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express();
 var bodyParser = require('body-parser');
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'client/build')))
+
 Quote = require('../models/quoteData')
 router.use(bodyParser.json());
 
